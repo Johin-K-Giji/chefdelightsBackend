93d7c73114dd847c7d41897832de70d595a78c2f
Johin123 <johinkgigi@gmail.com>
2025-05-13 12:10:34 +0530

    consoling image path

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,7 +20,8 @@ const createProduct = async (req, res) => {
       coverImage,
       subImages,
     });
-
+    console.log("Cover Image Filename:", coverImage); 
+    console.log("Cover Image Full Path:", path.join(__dirname, "..", "uploads", coverImage));
     await newProduct.save();
     res.status(201).json(newProduct);
   } catch (error) {
